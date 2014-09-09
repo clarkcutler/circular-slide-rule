20193d2cbf9f0d0b2ff181183ce0cf9dcd5275cd
Clark Cutler <clark@carbonfive.com>
2014-09-08 20:34:21 -0700

    1st circle with labels (positioned wrong)

--- scripts/circular_slide_rule.js
+++ scripts/circular_slide_rule.js
@@ -2,16 +2,13 @@ var width = 960,
     height = 500,
     radius = Math.min(width, height) / 2;
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
     .innerRadius(radius - 70);
 
 var pie = d3.layout.pie()
     .sort(null)
-    .value(function(d) { return d.population; });
+    .value(function(d) { return 1 });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -19,13 +16,7 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var data = [{"age":"<5","population":"1"},
- {"age":"5-13","population":"4499890"},
- {"age":"14-17","population":"2159981"},
- {"age":"18-24","population":"3853788"},
- {"age":"25-44","population":"14106543"},
- {"age":"45-64","population":"8819342"},
- {"age":"≥65","population":"612463"}]
+var data = [10,20,30,40,50,60,70,80,90];
 
 data.forEach(function(d) {
   d.population = +d.population;
@@ -37,12 +28,10 @@ var g = svg.selectAll(".arc")
     .attr("class", "arc");
 
 g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color(d.data.age); });
+    .attr("d", arc);
 
 g.append("text")
     .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
     .attr("dy", ".35em")
     .style("text-anchor", "middle")
-    .text(function(d) { return d.data.age; });
-
+    .text(function(d) { return d.data; });
